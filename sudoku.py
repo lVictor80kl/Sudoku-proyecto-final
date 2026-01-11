a071f86531f1a90dd4e20c91292243b9555a4804
Victor Marcano <152332746+lVictor80kl@users.noreply.github.com>
2026-01-11 18:09:14 -0400

    funcion para imprimir el sudoku mejor visualmente

--- sudoku.py
+++ sudoku.py
@@ -16,3 +16,23 @@ sudoku_inicial = [
 
 # TODO: implementar algoritmo genetico
 print("Sudoku inicial cargado")
+
+def mostrar_sudoku(tablero):
+    # funcion para ver el sudoku mas bonito
+    print("\n")
+    for i in range(9):
+        if i % 3 == 0 and i != 0:
+            print("------+-------+------")
+        linea = ""
+        for j in range(9):
+            if j % 3 == 0 and j != 0:
+                linea += "| "
+            if tablero[i][j] == 0:
+                linea += ". "
+            else:
+                linea += str(tablero[i][j]) + " "
+        print(linea)
+    print("\n")
+
+# probar la funcion
+mostrar_sudoku(sudoku_inicial)
